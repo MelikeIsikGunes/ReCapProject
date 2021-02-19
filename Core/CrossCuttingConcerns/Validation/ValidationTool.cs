00785adb8af79f452aee854214762ee982564947
Melike Işık <melike.34.37@hotmail.com>
2021-02-19 21:03:19 +0300

    FluentValidation desteği eklendi

---
+++ Core/CrossCuttingConcerns/Validation/ValidationTool.cs
@@ -0,0 +1,21 @@
+﻿using FluentValidation;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Core.CrossCuttingConcerns.Validation
+{
+    public class ValidationTool
+    {
+        public static void Validate(IValidator validator, object entity)
+        {
+            var context = new ValidationContext<object>(entity); //car türü için doğrulama contexti oluştur
+            var result = validator.Validate(context);
+            if (!result.IsValid) //sonuç geçerli değilse hata fırlat(doğrulama kurallarına uymuyorsa)
+            {
+                throw new ValidationException(result.Errors);
+            }
+        }
+        
+    }
+}
